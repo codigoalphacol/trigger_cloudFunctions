4a1d23ea804b94c8b3126b7b5182b61204feba28
CodigoAlphaCol <47418849+codigoalphacol@users.noreply.github.com>
2019-05-31 17:56:25 -0500

    Show the device token, Muestra el token del dispositivo

--- pages/messagin_page.dart
+++ pages/messagin_page.dart
@@ -1,39 +1,83 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-  
-  
-  class Messagin extends StatefulWidget {
-    @override
-    _MessaginState createState() => _MessaginState();
-  }
-  
-  class _MessaginState extends State<Messagin> {
-    final FirebaseMessaging _firebaseMessagin = FirebaseMessaging();
+import 'package:flutter/material.dart';
 
-    @override
+
+class MensajeToken extends StatefulWidget {
+  @override
+  _MensajeTokenState createState() => _MensajeTokenState();
+}
+class _MensajeTokenState extends State<MensajeToken> {
+
+  String _homeScreenText = "Waiting for token...";
+  String _messageText = "Waiting for message...";
+
+final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+@override
   void initState() {
     super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        setState(() {
+          _messageText = "Push Messaging message: $message";
+        });
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        setState(() {
+          _messageText = "Push Messaging message: $message";
+        });
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        setState(() {
+          _messageText = "Push Messaging message: $message";
+        });
+        print("onResume: $message");
+      },
+    );
 
-    _firebaseMessagin.getToken().then((token){
-      print(token);
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
     });
-    
+
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _homeScreenText = "Mi token es: $token";//envia el token a la pantalla
+      });
+      print(_homeScreenText);
+    });
   }
-
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-          appBar: AppBar(
-            title: Text('Message'),
-          ),
-          body: Column(
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Material(
+          child: Column(
             children: <Widget>[
               Container(
-                child: Text('Page Message', style: TextStyle(fontSize: 15.0),),
-              )
+                padding: EdgeInsets.all(20.0),
+                child: Center(
+                  child: Text(_homeScreenText),
+                ),
+              ),
+              Row(children: <Widget>[
+                Expanded(
+                  child: Text(_messageText),
+                ),
+              ])
             ],
-          ),
-      );
-    }
+),
+        ));
   }
+}
+void main() {
+  runApp(
+    MaterialApp(
+      home: MensajeToken(),
+    ),
+  );
+}
